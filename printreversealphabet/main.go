fe59e00ce272419a9dcfa9b44045677047121b3c
Akezhan1 <akezhka.lol@yandex.ru>
2019-10-16 10:11:34 +0600

    fina

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -3,8 +3,9 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	for i := 'z'; i <= 'a'; i++ {
+	var a rune = 122
+	for i := a; i >= 97; i-- {
 		z01.PrintRune(i)
 	}
-	z01.PrintRune('\n')
+	z01.PrintRune(10)
 }
