ecf0ff1d64f90b5acf864d744ea21ce6a40b7d6a
Akezhan1 <47324992+Akezhan1@users.noreply.github.com>
2019-10-16 10:00:00 +0600

    Update main.go

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -3,8 +3,7 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	var a rune = 'z'
-	for i := a; i <= 'a'; i++ {
+	for i := 'z; i <= 'a'; i-- {
 		z01.PrintRune(i)
 	}
 	z01.PrintRune(10)
