37428a5b90f2825bf75edef67477825ba096eab0
Akezhan1 <akezhka.lol@yandex.ru>
2019-10-17 09:12:30 +0600

    5

---
+++ printstr.go
@@ -0,0 +1,11 @@
+package printstr
+
+import "github.com/01-edu/z01"
+
+func PrintStr(str string) {
+	strrune := []rune(str)
+	for i := 0; i < len(strrune); i++ {
+		z01.PrintRune(strrune[i])
+	}
+	z01.PrintRune(10)
+}
