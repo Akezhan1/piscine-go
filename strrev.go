4791824b19595ac36c2165efb0cd0dfc1957380f
Akezhan1 <akezhka.lol@yandex.ru>
2019-10-17 11:29:16 +0600

    task 8

---
+++ strrev.go
@@ -0,0 +1,18 @@
+package strrev
+
+func StrRev(str string) string {
+	revstr := []rune(str)
+	strrune := []rune(str)
+	var a int
+
+	for i := range strrune {
+		a = i
+	}
+
+	for i := range revstr {
+		revstr[i] = strrune[a]
+		a--
+	}
+
+	return string(revstr)
+}
