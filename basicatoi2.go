f0805c6b2724b287ff27312e88903812379cfc1f
Akezhan1 <47324992+Akezhan1@users.noreply.github.com>
2019-10-17 18:47:33 +0600

    Update basicatoi2.go

--- basicatoi2.go
+++ basicatoi2.go
@@ -6,8 +6,8 @@ func BasicAtoi2(s string) int {
 		a := 0
 		if val > '1' && val < '9' {
 			for i := '1'; i <= val; i++ {
-			a++
-			res = res*10 + a
+				a++
+				res = res*10 + a
 			}
 		} else {
 			res = 0
