d575b41588178f171a8224221bc9b8c892d704ce
Akezhan1 <akezhka.lol@yandex.ru>
2019-10-18 12:37:57 +0600

    12

---
+++ sortintegertable.go
@@ -0,0 +1,21 @@
+package sortintgertable
+
+func SortIntegerTable(table []int) {
+	sort := false
+	var b int
+	for i := range table {
+		b = i
+	}
+	for !sort {
+		swap := false
+		for i := 0; i < b-1; i++ {
+			if table[i+1] < table[i] {
+				table[i+1], table[i] = table[i], table[i+1]
+				swap = true
+			}
+		}
+		if !swap {
+			sort = true
+		}
+	}
+}
