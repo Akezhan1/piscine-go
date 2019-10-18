41d2458d54fffe40943cb93870e9993e800977e8
Akezhan1 <47324992+Akezhan1@users.noreply.github.com>
2019-10-18 11:29:02 +0600

    Update atoi.go

--- atoi.go
+++ atoi.go
@@ -2,8 +2,11 @@ package atoi
 
 func Atoi(s string) int {
 	res := 0
-	for i, val := range s {
-		if s[i+1] == '+' || s[i+1] == '-' {
+	for _, val := range s {
+		if val == 32 {
+			return 0
+		}
+		if s[1] == '+' || s[1] == '-' {
 			return 0
 		}
 		if s[0] >= '0' && s[0] <= '9' || s[0] == '+' {
