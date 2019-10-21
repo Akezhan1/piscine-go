bd4065c3152a4672adf5080d5834f36599ecc1cd
Akezhan1 <47324992+Akezhan1@users.noreply.github.com>
2019-10-21 15:33:10 +0600

    Update iterativefactorial.go

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,7 +2,7 @@ package student
 
 func IterativeFactorial(y int) int {
 	res := 1
-	if res < 0 || res > 50 {
+	if y < 0 || y > 50 {
 		return 0
 	} else if y == 0 {
 		return 0
@@ -10,6 +10,10 @@ func IterativeFactorial(y int) int {
 		for i := 1; i <= y; i++ {
 			res = i * res
 		}
+	}
+	
+	if res < 0 {
+		return 0
 	}
 	return res
 }
