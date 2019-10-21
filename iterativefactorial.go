d0ecce5047ddc8116dfc223b218bcd3e13da51e9
Akezhan1 <akezhka.lol@yandex.ru>
2019-10-21 15:20:52 +0600

    04.01

---
+++ iterativefactorial.go
@@ -0,0 +1,16 @@
+package piscine
+
+func IterativeFactorial(y int) int {
+	res := 1
+	if y == 0 {
+		return 0
+	} else {
+		for i := 1; i <= y; i++ {
+			res = i*res
+		}
+	}
+	if res < 0 {
+		return 0
+	}
+	return res
+}
