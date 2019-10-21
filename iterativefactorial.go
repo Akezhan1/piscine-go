65933151b08613d1a7f1a6615773bb1d677b00de
Akezhan1 <akezhka.lol@yandex.ru>
2019-10-21 15:35:11 +0600

    04.01

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,12 +1,14 @@
-package piscine
+package student
 
 func IterativeFactorial(y int) int {
 	res := 1
-	if y == 0 {
+	if y < 0 || y > 50 {
 		return 0
+	} else if y == 0 {
+		return 1
 	} else {
 		for i := 1; i <= y; i++ {
-			res = i*res
+			res = i * res
 		}
 	}
 	if res < 0 {
