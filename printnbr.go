a43c626a6aca08f58de6fbe103d8093c406f9f3a
Akezhan1 <akezhka.lol@yandex.ru>
2019-10-21 18:00:26 +0600

    nbr

---
+++ printnbr.go
@@ -0,0 +1,31 @@
+package student
+
+import "github.com/01-edu/z01"
+
+func ToString(n int) string {
+	buf := [11]byte{}
+	pos := len(buf)
+	i := n
+	signed := i < 0
+	if signed {
+		i = -i
+	}
+	for {
+		pos--
+		buf[pos], i = '0'+byte(i%10), i/10
+		if i == 0 {
+			if signed {
+				pos--
+				buf[pos] = '-'
+			}
+			return string(buf[pos:])
+		}
+	}
+}
+
+func PrintNbr(n int) {
+	a := ToString(n)
+	for _, val := range a {
+		z01.PrintRune(val)
+	}
+}
