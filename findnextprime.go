f0487fad890371dc16fc336d876ccf2105ea516a
Akezhan1 <akezhka.lol@yandex.ru>
2019-10-21 16:11:55 +0600

    04.08

---
+++ findnextprime.go
@@ -0,0 +1,28 @@
+package student
+
+func IsPrime(nb int) bool {
+	if nb == 1 {
+		return false
+	}
+	if nb <= 0 {
+		return false
+	} else {
+		for i := 2; i*i <= nb; i++ {
+			if nb%i == 0 {
+				return false
+			}
+		}
+		return true
+	}
+}
+
+func FindNextPrime(n int) int {
+	for i:=0; i <= n; i++ { 
+		if IsPrime(n) == false {
+			n++
+		} else {
+			break
+		}
+	}
+	return n
+}
