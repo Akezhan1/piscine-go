85fc985cbf87c6facbe15f4bfa0648a5a62cb14c
Akezhan1 <47324992+Akezhan1@users.noreply.github.com>
2019-10-21 15:48:34 +0600

    Update recursivepower.go

--- recursivepower.go
+++ recursivepower.go
@@ -6,9 +6,7 @@ func RecursivePower(nb int, power int) int {
 		return 0
 	}
 	if nb == 0 {
-		return 0
-	} else if nb < 0 {
-		return 0
+		return 1
 	} else if power > 1 {
 		res = nb * res * RecursivePower(nb, power-1)
 	}
