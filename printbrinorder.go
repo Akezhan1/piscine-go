20d9e40a956d3859bf29ee908f11d1a07534a2ee
Akezhan1 <akezhka.lol@yandex.ru>
2019-10-22 10:15:34 +0600

    05.02

--- printbrinorder.go
+++ printbrinorder.go
@@ -4,6 +4,28 @@ import (
 	"github.com/01-edu/z01"
 )
 
+func SortIntegerTable(table []int) []int {
+	sort := false
+	var b int
+	for i := range table {
+		b = i
+	}
+	b = b + 1
+	for !sort {
+		swap := false
+		for i := 0; i < b-1; i++ {
+			if table[i+1] < table[i] {
+				table[i+1], table[i] = table[i], table[i+1]
+				swap = true
+			}
+		}
+		if !swap {
+			sort = true
+		}
+	}
+	return table
+}
+
 func ToMassive(n int) []int {
 	var res []int
 	for n > 0 {
@@ -24,7 +46,7 @@ func PrintNbr(n int) {
 		z01.PrintRune('0')
 	} else {
 		mod := '0'
-		for _, i := range ToMassive(n) {
+		for _, i := range SortIntegerTable(ToMassive(n)) {
 			for k := 0; k < i; k++ {
 				mod++
 			}
