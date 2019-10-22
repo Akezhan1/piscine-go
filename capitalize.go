7919f8792d01d187493ee6fc2329fc4e3889e0f3
Akezhan1 <akezhka.lol@yandex.ru>
2019-10-22 18:35:59 +0600

    05.11

--- capitalize.go
+++ capitalize.go
@@ -2,25 +2,33 @@ package student
 
 func Capitalize(s string) string {
 	srunes := []rune(s)
-	len := 0
-	var temp rune
-	for i := range s {
-		len = i + 1
-	}
-	for i := 0; i < len; i++ {
-		if srunes[i] == ' ' || srunes[i] == '+' || srunes[i] == '-' {
-			temp = srunes[i+1]
-			srunes[i+1] = ToUpper(temp)
-		} else {
-			srunes[i] = srunes[i]
+	ok := false
+	for i := 0; i < len(s); i++ {
+		if trust(srunes[i]) == true && ok {
+			if srunes[i] >= 'a' && srunes[i] <= 'z' {
+				srunes[i] = ToUpper(srunes[i])
+			}
+			ok = false
+		} else if srunes[i] >= 'A' && srunes[i] <= 'Z' {
+			srunes[i] = ToLower(srunes[i])
+		} else if trust(srunes[i]) == false {
+			ok = true
 		}
 	}
 	return string(srunes)
 }
 
-func ToUpper(s rune) rune {
-	if rune(s-32) < 65 {
-		return s
+func trust(a rune) bool {
+	if (a >= 'A' && a <= 'Z') || (a >= 'a' && a <= 'z') || (a >= '0' && a <= '9') {
+		return true
 	}
+	return false
+}
+
+func ToUpper(s rune) rune {
 	return rune(s - 32)
+}
+
+func ToLower(s rune) rune {
+	return rune(s + 32)
 }
