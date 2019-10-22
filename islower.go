70e8107aba36439dce7434cb8f529bff22f9535b
Akezhan1 <akezhka.lol@yandex.ru>
2019-10-22 20:17:10 +0600

    05.13

--- islower.go
+++ islower.go
@@ -1,31 +1,13 @@
 package student
 
-func IsAlpha(str string) bool {
-	len := 0
-	count := 0
-	for i := range str {
-		len = i
-	}
-	for i := 0; i <= len; i++ {
-		if str[i] >= 0 && str[i] <= 64 || str[i] >= 91 && str[i] <= 96 || str[i] >= 123 && str[i] <= 127 {
-			return false
-		} else {
-			if lowercase(rune(str[i])) {
-				count++
-			} else {
-				count = 0
-			}
+func IsALower(str string) bool {
+	runes := []rune(str)
+	ok := false
+	for _, i := range runes {
+		if !(i >= 'a' && r <= 'z') {
+			return ok
 		}
+		ok = true
 	}
-	if count-1 == len {
-		return true
-	}
-	return false
-}
-
-func lowercase(s rune) bool {
-	if s+32 > 122 {
-		return true
-	}
-	return false
+	return ok
 }
