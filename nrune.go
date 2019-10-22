85d08d88c681e4bd671ecdcef0d8372ea515ec0e
Akezhan1 <akezhka.lol@yandex.ru>
2019-10-22 13:07:15 +0600

    05.05

---
+++ nrune.go
@@ -0,0 +1,17 @@
+package student
+
+func NRune(s string, n int) rune {
+	runes := []rune(s)
+	len := 0
+	final := 0
+	for i := range runes {
+		len = i
+	}
+	if n > len {
+		return 0
+	}
+	for i := 1; i <= n; i++ {
+		final = n
+	}
+	return runes[final]
+}
