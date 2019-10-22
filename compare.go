36e77bd18f281172041c4cc2dcc1cf78f76889a6
Akezhan1 <akezhka.lol@yandex.ru>
2019-10-22 16:04:05 +0600

    05.08

---
+++ compare.go
@@ -0,0 +1,42 @@
+package student
+
+func Compare(a, b string) int {
+	arunes := []rune(a)
+	brunes := []rune(b)
+	count := 0
+	len := 0
+	for i := range a {
+		len = i + 1
+	}
+	for i := range brunes {
+		if IndexRune(arunes, brunes[i]) {
+			count++
+		}
+	}
+	if len == count {
+		return 0
+	}
+
+	for i := range a {
+		for k := range b {
+			switch {
+			case a[i] > b[k]:
+				return 1
+			case a[i] < b[k]:
+				return -1
+			}
+		}
+	}
+	return len
+}
+
+func IndexRune(s []rune, r rune) bool {
+	indexrune := false
+	for i := range s {
+		if s[i] == r {
+			indexrune = true
+			return indexrune
+		}
+	}
+	return indexrune
+}
