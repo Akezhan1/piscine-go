c48b164120dab28b9720fc9e34cf08f60524082a
Akezhan1 <47324992+Akezhan1@users.noreply.github.com>
2019-10-22 17:20:18 +0600

    Update capitalize.go

--- capitalize.go
+++ capitalize.go
@@ -3,13 +3,14 @@ package student
 func Capitalize(s string) string {
 	srunes := []rune(s)
 	len := 0
+	var temp rune
 	for i := range s {
 		len = i + 1
 	}
 	for i := 0; i < len; i++ {
 		if srunes[i] == ' ' || srunes[i] == '+' || srunes[i] == '-' {
-			temp := srunes[i+1]
-			srunes[i+1] = ToUpper(rune(temp))
+			temp = srunes[i+1]
+			srunes[i+1] = ToUpper(temp)
 		} else {
 			srunes[i] = srunes[i]
 		}
