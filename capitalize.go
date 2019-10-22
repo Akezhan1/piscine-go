86f0de1bdb9157b62a68c5f1ba6657934e0fbc5e
Akezhan1 <47324992+Akezhan1@users.noreply.github.com>
2019-10-22 19:17:29 +0600

    Update capitalize.go

--- capitalize.go
+++ capitalize.go
@@ -8,10 +8,16 @@ func trust(a rune) bool {
 }
 
 func ToUpper(s rune) rune {
+	if s - 32 < 64 {
+		return s
+	}
 	return rune(s - 32)
 }
 
 func ToLower(s rune) rune {
+	if s + 32 > 122 {
+		return s	
+	}
 	return rune(s + 32)
 }
 
