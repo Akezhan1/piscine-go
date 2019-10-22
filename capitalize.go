55f384c5d1c0d59b6385d0d15afddb5f0212f2a2
Akezhan1 <47324992+Akezhan1@users.noreply.github.com>
2019-10-22 19:24:44 +0600

    Update capitalize.go

--- capitalize.go
+++ capitalize.go
@@ -1,47 +1,27 @@
 package student
 
-func trust(a rune) bool {
-	if (a >= 'A' && a <= 'Z') || (a >= 'a' && a <= 'z') || (a >= '0' && a <= '9') {
-		return true
-	}
-	return false
-}
-
-func ToUpper(s rune) rune {
-	if s-32 < 65 {
-		return s
-	}
-	return rune(s - 32)
-}
-
-func ToLower(s rune) rune {
-	if s+32 > 122 {
-		return s
-	}
-	return rune(s + 32)
-}
-
 func Capitalize(s string) string {
-	srunes := []rune(s)
-	len := 0
-	ok := true
-	for i := range s {
-		len = i
+	runes := []rune(s)
+
+	strlen := 0
+	for i := range runes {
+		strlen = i + 1
 	}
 
-	for i := 0; i < len; i++ {
-		if trust(srunes[i]) == true && ok {
-			if srunes[i] >= 'a' && srunes[i] <= 'z' {
-				srunes[i] = ToUpper(srunes[i])
-			} else if srunes[i] >= '0' && srunes[i] <= '9' {
-				srunes[i+1] = ToLower(srunes[i+1])
+	for i := 0; i < strlen; i++ {
+		if i != 0 && (!((runes[i-1] >= 'a' && runes[i-1] <= 'z') || (runes[i-1] >= 'A' && runes[i-1] <= 'Z') || (runes[i-1] >= '0' && runes[i-1] <= '9'))) {
+			if runes[i] >= 'a' && runes[i] <= 'z' {
+				runes[i] = rune(runes[i] - 32)
 			}
-			ok = false
-		} else if srunes[i] >= 'A' && srunes[i] <= 'Z' {
-			srunes[i] = ToLower(srunes[i])
-		} else if trust(srunes[i]) == false {
-			ok = true
+		} else if i == 0 {
+			if runes[i] >= 'a' && runes[i] <= 'z' {
+				runes[i] = rune(runes[i] - 32)
+			}
+		} else {
+			if runes[i] >= 'A' && runes[i] <= 'Z' {
+				runes[i] = rune(runes[i] + 32)
+			}
 		}
 	}
-	return string(srunes)
+	return string(runes)
 }
