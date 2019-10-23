f89597f9bc5aa03ada7f6864fc39bc8c7ca6e437
Akezhan1 <akezhka.lol@yandex.ru>
2019-10-23 08:17:32 +0600

    06.04

---
+++ sortparams/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	arguments := os.Args
+	len := 0
+	sort := false
+	for i := range arguments {
+		len = i
+	}
+	for i := 1; i <= len; i++ {
+		for _, k := range arguments[i] {
+			for !sort {
+				swap := false
+				for i := 0; i < len; i++ {
+					if arguments[i+1] < arguments[i] {
+						arguments[i+1], arguments[i] = arguments[i], arguments[i+1]
+						swap = true
+					}
+				}
+				if !swap {
+					sort = true
+				}
+			}
+			z01.PrintRune(k)
+		}
+		z01.PrintRune(10)
+	}
+}
