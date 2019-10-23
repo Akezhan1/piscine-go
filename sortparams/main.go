29a185202a17e4df432a1eaeed95c40eda8072bf
Akezhan1 <akezhka.lol@yandex.ru>
2019-10-23 08:29:02 +0600

    06.04

--- sortparams/main.go
+++ sortparams/main.go
@@ -9,24 +9,20 @@ import (
 func main() {
 	arguments := os.Args
 	len := 0
-	sort := false
 	for i := range arguments {
 		len = i
 	}
+
+	for i := 1; i < len; i++ {
+		for j := i + 1; j < len; j++ {
+			if arguments[i] > arguments[j] {
+				arguments[i], arguments[j] = arguments[j], arguments[i]
+			}
+		}
+	}
+
 	for i := 1; i <= len; i++ {
 		for _, k := range arguments[i] {
-			for !sort {
-				swap := false
-				for i := 0; i < len; i++ {
-					if arguments[i+1] < arguments[i] {
-						arguments[i+1], arguments[i] = arguments[i], arguments[i+1]
-						swap = true
-					}
-				}
-				if !swap {
-					sort = true
-				}
-			}
 			z01.PrintRune(k)
 		}
 		z01.PrintRune(10)
