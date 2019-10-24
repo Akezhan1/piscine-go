f0c9bbf3cbb25d15233a7c1d3215a566313b8bc8
Akezhan1 <akezhka.lol@yandex.ru>
2019-10-24 08:52:51 +0600

    07.01

---
+++ appendrange.go
@@ -0,0 +1,14 @@
+package student
+
+func AppendRange(min, max int) []int {
+	var res []int
+	if min > max {
+		return res
+	} else if min == max {
+		return res
+	}
+	for i := min; i < max; i++ {
+		res = append(res, i)
+	}
+	return res
+}
