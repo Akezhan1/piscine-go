eb154afbdc02f72470ff86a3c11cdc08db6002ee
Akezhan1 <akezhka.lol@yandex.ru>
2019-10-24 16:14:56 +0600

    07.04

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,4 +1,4 @@
-package student
+package main
 
 func SplitWhiteSpaces(str string) []string {
 	len := 0
@@ -8,21 +8,21 @@ func SplitWhiteSpaces(str string) []string {
 			len++
 		}
 	}
-	for i := 0; i < len; i++ {
-		if str[0] == ' ' {
-			len--
-		} else if str[len] == ' ' {
-			len--
-		}
+	len++
+	if str[0] == ' ' {
+		len--
+	}
+	if str[len] == ' ' {
+		len--
 	}
 	for i := range str {
 		len2 = i
 	}
-	len++
+
 	res := make([]string, len)
 	check := true
 	k := 0
-	for i := 0; i < len2; i++ {
+	for i := 0; i <= len2; i++ {
 		if str[i] == ' ' || str[i] == '\t' || str[i] == '\n' {
 			if !check {
 				k++
